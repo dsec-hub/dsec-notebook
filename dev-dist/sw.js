4135cdca3aad950a417da329ae011d90e9b8bdf9
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2026-09-01 15:53:43 +0800

    updated fonts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.5d3f7lf01og"
+    "revision": "0.or5ntbjovsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
